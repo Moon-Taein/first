974078c3b8926798d9236dd3fad000ef2a359d8d
Moon-Taein <mji3988@gmail.com>
2023-05-25 10:09:23 +0900

    첫번째 문장 작업

--- vcs test/src/Hello.java
+++ vcs test/src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
 	public static void main(String[] args) {
+		// 로컬에서 작업했습니다.
 		System.out.println("Hello World");
 
 		System.out.println("변경");
@@ -8,5 +9,6 @@ public class Hello {
 
 		System.out.println("네번째 커밋");
 		// 주석
+
 	}
 }
