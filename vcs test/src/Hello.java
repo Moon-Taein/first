431e9c53b68ad155e6790794402bcc5bf181d439
Moon-Taein <51267486+Moon-Taein@users.noreply.github.com>
2023-05-25 10:26:26 +0900

    Update Hello.java
    ㅁㅇㅁㅈㅇㅁㅈ

--- vcs test/src/Hello.java
+++ vcs test/src/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// 원격에서 작업했습니다.
 		// 원격에서 작업했습니다.
+		/ㅇ/ㅈ/ㅇㅈ/ㅇ/ㅈ/ㅇ/ㅈ/ㅇㅈ/ㅇ
 		System.out.println("Hello World");
 
 		System.out.println("변경");
